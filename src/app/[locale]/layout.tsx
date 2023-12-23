fe899170ed63711705e266270a6e3b2b3fed56db
Joel Lappalainen <joel.lappalainen@gmail.com>
2023-12-23 12:49:10 +0200

    fix: iphone auto zoom on inputs

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,17 @@
-import type { Metadata, Viewport } from "next";
-import { Montserrat } from "next/font/google";
-import "../globals.css";
-import { ThemeProvider } from "@/components/ThemeProvider";
-import { getDictionary } from "@/lib/dictionaries";
+import type { Metadata, Viewport } from 'next';
+import { Montserrat } from 'next/font/google';
+import '../globals.css';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import { getDictionary } from '@/lib/dictionaries';
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ['latin'] });
 
 type Props = {
   params: { locale: string };
 };
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const { locale = "en" } = params;
+  const { locale = 'en' } = params;
   const dictionary = await getDictionary(locale);
   const title = dictionary.title;
   const appDescription = dictionary.appDescription;
@@ -19,22 +19,23 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   return {
     title,
     description: appDescription,
-    manifest: "/manifest.json",
+    manifest: '/manifest.json',
     authors: {
-      name: "Joel Lappalainen",
+      name: 'Joel Lappalainen',
     },
   };
 }
 
 export const viewport: Viewport = {
-  themeColor: "#ffe4c2",
-  width: "device-width",
+  themeColor: '#ffe4c2',
+  width: 'device-width',
   initialScale: 1,
   minimumScale: 1,
+  maximumScale: 1,
 };
 
 export async function generateStaticParams() {
-  return [{ locale: "fi" }, { locale: "en" }];
+  return [{ locale: 'fi' }, { locale: 'en' }];
 }
 
 export default async function RootLayout({
